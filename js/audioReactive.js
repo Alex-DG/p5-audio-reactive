a4f1d623e1724d4e05cc585addf056ced31a7fc5
Alex Di Guida <alexandre.diguida@gmail.com>
2022-07-13 11:35:38 +0100

    Image distortion based on audio frequency

--- js/audioReactive.js
+++ js/audioReactive.js
@@ -1,41 +1,21 @@
 import audioSrc from '../assets/audio/02.mp3'
+import imageSrc from '../assets/images/01.jpg'
 
 const start = (sketch) => {
-  let myShaders
+  let myShader
   let audio
+  let img
   let canvas
-  let amp
+  let amplitude
   let fft
   let beatDetect
   let bgColor = 0
-
-  let words = [
-    'melt',
-    'fear',
-    'bounce',
-    'rabid',
-    'fuzzy',
-    'gratis',
-    'sense',
-    'madly',
-    'yellow',
-    'crazy',
-    'ahead',
-    'super',
-    'classy',
-    'craven',
-    'sassy',
-    'roomy',
-    'sedate',
-    'gaudy',
-    'moldy',
-    'groovy',
-  ]
 
   const texts = [...document.querySelectorAll('.text span')]
 
   let isShaderLoaded = false
   let isAudioLoaded = false
+  let isImageLoaded = false
   let isReady = false
 
   const togglePlay = () => {
@@ -50,22 +30,20 @@ const start = (sketch) => {
 
   const triggerBeat = () => {
     bgColor = color(random(255), random(255), random(255))
-
-    texts.forEach((el, i) => {
-      setTimeout(() => {
-        el.innerHTML = random(words)
-      }, random(200) * i)
-    })
   }
 
   const onShaderLoaded = () => {
     isShaderLoaded = true
   }
-  const onAudioLoader = () => {
+  const onAudioLoaded = () => {
     isAudioLoaded = true
   }
+  const onImageLoaded = () => {
+    isImageLoaded = true
+  }
   const initDom = () => {
-    const isInitDom = !isReady && isShaderLoaded && isAudioLoaded
+    const isPreloadReady = isShaderLoaded && isAudioLoaded && isImageLoaded
+    const isInitDom = !isReady && isPreloadReady
     if (isInitDom) {
       isReady = true
       const titleDom = document.getElementById('title')
@@ -83,51 +61,49 @@ const start = (sketch) => {
     const vertex = new URL('./shaders/vertex.glsl', import.meta.url)
     const fragment = new URL('./shaders/fragment.glsl', import.meta.url)
 
-    myShaders = loadShader(vertex, fragment, onShaderLoaded)
-    audio = loadSound(audioSrc, onAudioLoader)
+    myShader = loadShader(vertex, fragment, onShaderLoaded)
+    audio = loadSound(audioSrc, onAudioLoaded)
+    img = loadImage(imageSrc, onImageLoaded)
   }
 
   sketch.setup = () => {
     canvas = createCanvas(windowWidth, windowHeight, WEBGL)
     canvas.mouseClicked(togglePlay)
 
-    amp = new p5.Amplitude()
+    amplitude = new p5.Amplitude()
     fft = new p5.FFT()
 
     beatDetect = new p5.PeakDetect(20, 20000, 0.2)
     beatDetect.onPeak(triggerBeat)
 
-    shader(myShaders)
+    shader(myShader)
+
+    myShader.setUniform('uTexture', img)
+    myShader.setUniform('uResolution', [width, height])
+    myShader.setUniform('uTextureResolution', [img.width, img.height])
   }
 
   sketch.draw = () => {
     initDom()
 
-    background(bgColor)
+    background(0)
 
-    fft.analyze()
-    beatDetect.update(fft)
+    // Start FFT
+    fft.analyze() // 0 -> 255
 
-    const volume = amp.getLevel() // 0 - 1
-
-    // Spectral Centroid Frequency of the spectral centroid in Hz.
+    const volume = amplitude.getLevel()
     let freq = fft.getCentroid()
     freq *= 0.001
 
-    const mapCentroid = map(freq, 0, 5, -1, 1)
-    const mapFreq = map(fft.getEnergy('mid'), 0, 100, 0, 0.1)
+    const mapA = map(volume, 0, 1, 0, 0.05)
+    const mapF = map(freq, 0, 1, 0, 10)
 
-    const rotation = map(audio.currentTime(), 0, audio.duration(), 0, 0.001)
+    myShader.setUniform('uTime', frameCount)
 
-    myShaders.setUniform('uAmplitude', mapCentroid)
-    myShaders.setUniform('uFrequency', mapFreq)
+    myShader.setUniform('uFrequency', mapF)
+    myShader.setUniform('uAmp', mapA)
 
-    myShaders.setUniform('uTime', frameCount * 0.001)
-
-    rotateY(frameCount * rotation)
-
-    // Maybe try to control the size of the sphere with the volume?
-    sphere(width / 8, 50, 50)
+    rect(0, 0, width, height)
   }
 
   sketch.windowResized = () => {
--- js/shaders/fragment.glsl
+++ js/shaders/fragment.glsl
@@ -1,24 +1,52 @@
 precision mediump float;
 
 varying vec2 vTexCoord;
-varying vec3 vNormal;
+
+uniform sampler2D uTexture;
+uniform vec2 uTextureResolution;
+
+uniform vec2 uResolution;
+
 uniform float uTime;
-varying float vNoise;
-varying float vDisplacement;
+uniform float uFrequency;
+uniform float uAmp;
 
-float PI = 3.14159265358979323846264;
-
-// https://iquilezles.org/www/articles/palettes/palettes.htm
-vec3 palette( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
-{
-    return a + b*cos( 6.28318*(c*t+d) );
+mat2 scale(vec2 _scale) {
+  return mat2(_scale.x, 0.0, 0.0, _scale.y);
 }
 
-void main() {
-  vec3 color = vec3(vTexCoord.y * 1.2, vTexCoord.y * 1.2501, vTexCoord.y * 1.312);
-  
-  float diffuse = 0.004 * max(dot(vTexCoord, vec2(10.0)), 21.0);
-  color = pow(color, vec3(10.0 * vNoise));
+// void main() {
+//   vec3 color = vec3(vTexCoord.y);
 
-  gl_FragColor = vec4(color * diffuse, 1.0);
+//   gl_FragColor = vec4(color, 1.0);
+// }
+
+void main() {
+
+  vec2 ratio = vec2(
+    min((uResolution.x / uResolution.y) / (uTextureResolution.x / uTextureResolution.y), 1.0),
+    min((uResolution.y / uResolution.x) / (uTextureResolution.y / uTextureResolution.x), 1.0)
+  );
+
+  vec2 uv = vec2(
+    vTexCoord.x * ratio.x + (1.0 - ratio.x) * 0.5,
+    vTexCoord.y * ratio.y + (1.0 - ratio.y) * 0.5
+  );
+
+  uv.y = 1.0 - uv.y;
+
+  uv -= vec2(0.5);
+  uv = scale(vec2(0.91)) * uv;
+  uv += vec2(0.5);
+
+  float frequency = uFrequency;
+  float amplitude = uAmp;
+
+  float distortion = sin(uv.y * frequency + (uTime * 0.01)) * amplitude;
+  // float distortion = sin(uv.x * frequency + (uTime * 0.1)) * amplitude;
+  // float distortion = sin(uv.y * frequency + (uTime * 0.2)) * amplitude;
+
+  vec4 texture = texture2D(uTexture, uv + distortion);
+
+  gl_FragColor = texture;
 }
